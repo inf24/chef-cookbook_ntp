9b968c8dd5d1807b0dd2f2205322ba85b3be9e66
Maximilian Sparenberg <maximilian.sparenberg@gmail.com>
2015-11-30 15:15:21 +0100

    - installiere ntp - konfiguriere ntp - starte/autostart ntp - erste tests

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,3 +2,25 @@
 # Maximilian Sparenberg | Max.Sparenberg@icloud.com | inf24
 ##
 
+default['ntp']['timeservers'] = %w{
+  0.de.pool.ntp.org
+	1.de.pool.ntp.org
+	2.de.pool.ntp.org
+	3.de.pool.ntp.org
+}
+
+default['ntp']['package'] = 'ntp'
+default['ntp']['conf']['file'] = '/etc/ntp.conf'
+
+case node['platform']
+when 'centos'
+  if node['platform_version'].to_f >= 6.5
+    default['ntp']['service'] = 'ntpd'
+    default['ntp']['conf']['dir'] = '/etc/ntp/'
+  end
+when 'suse'
+  if node['platform_version'].to_f >= 11.0
+    default['ntp']['service'] = 'ntp'
+    default['ntp']['conf']['dir'] = '/etc/'
+  end
+end
--- recipes/default.rb
+++ recipes/default.rb
@@ -2,3 +2,21 @@
 # Maximilian Sparenberg | Max.Sparenberg@icloud.com | inf24
 ##
 
+package = node['ntp']['package']
+service = node['ntp']['service']
+file = node['ntp']['conf']['file']
+platform = node['platform']
+
+package "#{node['ntp']['package']}" do
+  action :install
+end
+
+template "#{node['ntp']['conf']['file']}" do
+  source "ntp.conf_#{platform}.erb"
+  action :create
+end
+
+service "#{node['ntp']['service']}" do
+  supports :status => true, :start => true, :restart => true, :stop => false
+  action [ :enable, :start ]
+end
