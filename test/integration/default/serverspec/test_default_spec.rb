592feda7fdf50b610a82525dc7b112fe3a75dfde
Maximilian Sparenberg <maximilian.sparenberg@gmail.com>
2015-12-02 21:52:28 +0100

    Serverspec tests hinzugefügt

--- test/integration/default/serverspec/test_default_spec.rb
+++ test/integration/default/serverspec/test_default_spec.rb
@@ -8,3 +8,16 @@ describe command('/bin/sh --version') do
   its(:exit_status) { should eq 0 }
 end
 
+describe package('ntp') do
+  it { should be_installed}
+end
+describe service('ntpd') do
+  it { should be_running }
+  it { should be_enabled }
+end
+describe file('/etc/ntp.conf') do
+  it { should be_file }
+end
+describe port(123) do
+  it { should be_listening }
+end
